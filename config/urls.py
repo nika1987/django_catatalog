0e7b543ce5efddba1a0ee285492f00647e2cef91
veronika <@Nika_Zolotova>
2023-10-16 00:58:23 +0400

    update blog

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls', namespace='блог'))
+
 ]
